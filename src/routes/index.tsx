fe911b38dcc4918358833d19fd02ae5e8f208d42
Dubzer <me@dubzer.dev>
2025-11-15 02:13:06 +0300

    fix repro by using highlighted code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,40 +1,42 @@
 import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({
-  component: Home,
-})
-
 const code = `{
-    "description": "caps_lock to russian <-> english",
-    "manipulators": [
+    <span class="hljs-string">&quot;description&quot;</span>: <span class="hljs-string">&quot;caps_lock to russian &lt;-&gt; english&quot;</span>,
+    <span class="hljs-string">&quot;manipulators&quot;</span>: [
         {
-            "conditions": [
+            <span class="hljs-string">&quot;conditions&quot;</span>: [
                 {
-                    "input_sources": [{ "input_source_id": "^com\\\\.apple\\\\.keylayout\\\\.RussianWin$" }],
-                    "type": "input_source_if"
+                    <span class="hljs-string">&quot;input_sources&quot;</span>: [{ <span class="hljs-string">&quot;input_source_id&quot;</span>: <span class="hljs-string">&quot;^com<span class="hljs-subst">\\\\</span>.apple<span class="hljs-subst">\\\\</span>.keylayout<span class="hljs-subst">\\\\</span>.RussianWin$&quot;</span> }],
+                    <span class="hljs-string">&quot;type&quot;</span>: <span class="hljs-string">&quot;input_source_if&quot;</span>
                 }
             ],
-            "from": { "key_code": "caps_lock" },
-            "to": [{ "select_input_source": { "input_source_id": "^com\\\\.apple\\\\.keylayout\\\\.US$" } }],
-            "type": "basic"
+            <span class="hljs-string">&quot;from&quot;</span>: { <span class="hljs-string">&quot;key_code&quot;</span>: <span class="hljs-string">&quot;caps_lock&quot;</span> },
+            <span class="hljs-string">&quot;to&quot;</span>: [{ <span class="hljs-string">&quot;select_input_source&quot;</span>: { <span class="hljs-string">&quot;input_source_id&quot;</span>: <span class="hljs-string">&quot;^com<span class="hljs-subst">\\\\</span>.apple<span class="hljs-subst">\\\\</span>.keylayout<span class="hljs-subst">\\\\</span>.US$&quot;</span> } }],
+            <span class="hljs-string">&quot;type&quot;</span>: <span class="hljs-string">&quot;basic&quot;</span>
         },
         {
-            "conditions": [
+            <span class="hljs-string">&quot;conditions&quot;</span>: [
                 {
-                    "input_sources": [{ "input_source_id": "^com\\\\.apple\\\\.keylayout\\\\.US$" }],
-                    "type": "input_source_if"
+                    <span class="hljs-string">&quot;input_sources&quot;</span>: [{ <span class="hljs-string">&quot;input_source_id&quot;</span>: <span class="hljs-string">&quot;^com<span class="hljs-subst">\\\\</span>.apple<span class="hljs-subst">\\\\</span>.keylayout<span class="hljs-subst">\\\\</span>.US$&quot;</span> }],
+                    <span class="hljs-string">&quot;type&quot;</span>: <span class="hljs-string">&quot;input_source_if&quot;</span>
                 }
             ],
-            "from": { "key_code": "caps_lock" },
-            "to": [{ "select_input_source": { "input_source_id": "^com\\\\.apple\\\\.keylayout\\\\.RussianWin$" } }],
-            "type": "basic"
+            <span class="hljs-string">&quot;from&quot;</span>: { <span class="hljs-string">&quot;key_code&quot;</span>: <span class="hljs-string">&quot;caps_lock&quot;</span> },
+            <span class="hljs-string">&quot;to&quot;</span>: [{ <span class="hljs-string">&quot;select_input_source&quot;</span>: { <span class="hljs-string">&quot;input_source_id&quot;</span>: <span class="hljs-string">&quot;^com<span class="hljs-subst">\\\\</span>.apple<span class="hljs-subst">\\\\</span>.keylayout<span class="hljs-subst">\\\\</span>.RussianWin$&quot;</span> } }],
+            <span class="hljs-string">&quot;type&quot;</span>: <span class="hljs-string">&quot;basic&quot;</span>
         }
     ]
 }`
 
+export const Route = createFileRoute('/')({
+  component: Home,
+  loader: () => code
+})
+
+
 function Home() {
+  const data = Route.useLoaderData();
   return (
-    <div className="p-2" dangerouslySetInnerHTML={{ __html: code }}>
+    <div className="p-2" dangerouslySetInnerHTML={{ __html: data }}>
     </div>
   )
 }
